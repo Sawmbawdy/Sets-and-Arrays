d66c151a391d64e3e1d4bbadc2d0f0d1342ff8f8
Sawmbawdy <saiompatro12@gmail.com>
2025-10-02 19:16:44 -0400

    Add files via upload

---
+++ OperationOfSets.py
@@ -0,0 +1,14 @@
+import array as arr
+# Creating a set
+Integer_set={0,1,2,3,3,4,5,5,6}
+print(Integer_set)
+Integer_set2 = {1,2,3,4,5,6}
+print(Integer_set2)
+My_set={1,"hello",3.5,False}
+print(My_set)
+My_list = [1,2,3,4,5,5,6]
+print(set(My_list))
+Integer_set.pop()
+print(Integer_set)
+Integer_set.remove(3)
+print(Integer_set)
